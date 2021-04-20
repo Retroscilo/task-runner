ad823ff1813bb107ed53705b3c0ad5bcb5672653
Loïc Torres <loic.torresg@gmail.com>
2021-04-20 02:41:42 +0200

    pagination

--- Screens/Home.js
+++ Screens/Home.js
@@ -5,12 +5,48 @@ import { find } from '../lib/user'
 import { ActivityIndicator, Colors } from 'react-native-paper';
 import { Avatar, Card, IconButton } from 'react-native-paper';
 
-const pagination = (props) => {
+const Pagination = ({ setDisplayedUsers }) => {
+  const [ users, setUsers ] = useState(null)
+  useEffect(() => {
+    async function fetchData() {
+      // fetch users
+      const res = await find()
+      setUsers(res)
+    }
+    fetchData()
+  }, [])
+
+  const [ pages, setPages ] = useState(null)
+  useEffect(() => {
+    if (!users) return
+    // set number of pages in an array / ex: [ 1, 2 ] for 2 pages
+    let p = []
+    for (let i = 1; i <= Math.floor(users.length / 5); i++) p.push(i)
+    setPages(p)
+    // display first users
+    handlePageChange(1)
+  }, [ users ])
+
+  
+
+  function handlePageChange (page, u)  {
+    let displayedUsers = []
+    for(let i = 5*(page-1); i < 5*(page-1)+5; i++) displayedUsers.push(u || users[i])
+    setDisplayedUsers(displayedUsers)
+  }
+
+  if (!pages) return <ActivityIndicator animating={true} color={Colors.red800} />
   return (
-    <div
-      style={{ display: 'flex', justifyContent: 'space-between' }}
-    >
-      {}
+    <div style={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', height: 30 }}>
+      {pages.length > 0 && pages.map(i => {
+        return (
+        <span
+          key={i}
+          style={{ cursor: 'pointer' }}
+          onClick={() => handlePageChange(i)}
+        >
+          {i}
+        </span>)})}
     </div>
   )
 }
@@ -20,15 +56,7 @@ function HomeScreen() {
 
   const onChangeSearch = query => setSearchQuery(query);
 
-  const [ users, setUsers ] = useState(undefined)
-  useEffect(() => {
-    async function fetchData() {
-      const users = await find()
-      console.log(users)
-      setUsers(users)
-    }
-    fetchData()
-  }, [])
+  const [ displayedUsers, setDisplayedUsers ] = useState(undefined)
   
   return (
     <View>
@@ -38,16 +66,22 @@ function HomeScreen() {
         value={searchQuery}
         style={{ height: 55 }}
       />
-      {!users && <ActivityIndicator animating={true} color={Colors.red800} />}
-      {users && users.map((user, i) =>
-        <Card.Title
-          key={i}
-          title={user.name}
-          titleStyle={{ color: 'black', fontSize: 30, marginTop: 25 }}
-          left={(props) => <Avatar.Text {...props} label={user.initials} size= {67} />}
-          style={{ border: '1px solid lightgrey', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', paddingLeft: 0, paddingTop: 40, paddingBottom: 40, cursor: 'pointer', background: 'white' }}
-        />
-      )}
+      {displayedUsers && 
+      <span>
+        {displayedUsers.map((user, i) => {
+          if (i >= 5) return
+          return (
+          <Card.Title
+            key={i}
+            title={user.name}
+            titleStyle={{ color: 'black', fontSize: 30, marginTop: 25 }}
+            left={(props) => <Avatar.Text {...props} label={user.initials} size= {67} />}
+            style={{ border: '1px solid lightgrey', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', paddingLeft: 0, paddingTop: 40, paddingBottom: 40, cursor: 'pointer', background: 'white' }}
+          />)
+        })}
+      </span>
+      }
+      <Pagination setDisplayedUsers={setDisplayedUsers} />
     </View>
   );
 }
